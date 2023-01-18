f93cdbe06f13195ab759dbec2f1dc79d3a40c6e7
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-01-18 22:21:31 +0900

    230118 study finish
    mvvm 모델을 기반으로 간단한 이미지 뽑기 구현

---
+++ app/src/main/java/com/example/app/view/SetMaxActivity.kt
@@ -0,0 +1,37 @@
+package com.example.app.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.example.app.R
+import com.example.app.viewModel.MainViewModel
+import com.example.app.databinding.ActivitySetnumBinding
+
+
+class SetMaxActivity : AppCompatActivity() {
+
+    private lateinit var mbind : ActivitySetnumBinding
+    private val model : MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mbind = DataBindingUtil.setContentView(this,R.layout.activity_setnum)
+
+        mbind.lifecycleOwner = this
+        mbind.viewmodelSetMaxNum = MainViewModel()
+
+        var max : EditText = findViewById(R.id.maxnum)
+        var completeBtn : Button = findViewById(R.id.complete)
+
+        completeBtn.setOnClickListener{
+            model.setMaxNum(Integer.parseInt(max.text.toString()))
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
--- app/src/main/java/com/example/app/viewModel/MainViewModel.kt
+++ app/src/main/java/com/example/app/viewModel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.app.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.app.module.Database
 import java.util.*
@@ -11,5 +12,39 @@ class MainViewModel : ViewModel() {
 
     companion object{
         var MaxNum = 101
+    }
+
+    var text: MutableLiveData<String> = MutableLiveData()
+
+    init {
+        text.value = ""
+    }
+    fun getNum(){
+        nowNum = rand.nextInt(MaxNum) + 1
+        database.saveNumber(nowNum)
+        text.value = nowNum.toString()
+    }
+
+    fun setMaxNum(int: Int?){
+        println(int)
+        MaxNum = int!!
+    }
+
+    fun showAllNum() {
+        if (nowNum == -1)
+            return
+
+        text.value = ""
+        var array: ArrayList<Int> = ArrayList()
+
+        array = database.getAllNumber()
+
+
+        for (i in 0 until array.size) {
+            text.value += array.get(i).toString() + " "
+        }
+
+        println(text)
+
     }
 }
